2d10b7b63e3468c29e70915ab6295ba507790cdf
fansong1983 <fansong1983@163.com>
2021-01-10 20:56:01 +0800

    This code is only for self study.

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('helloworld!')
+if 5 > 2:
+    print('5 is greater than 2.')
+
+if 2 > 5:
+    print('Error.')
+x = 5
+y = 'Test'
+print(x, y)
+"""
+This is a multi-line comments
+Because no variable is assigned to this string!
+"""
