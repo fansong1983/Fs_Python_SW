7536fe6287a4faf24f87facb88ffb5d49a241782
fansong1983 <fansong1983@163.com>
2021-01-11 11:32:04 +0800

    This code is only for self study.

---
+++ Demo_2020_01_11.py
@@ -0,0 +1,16 @@
+"""
+This is a demo for List Tuple Set and Dictionary.
+Date:2021_01_11
+Written by Dr. Fan Song.
+This demo is only for self studying.
+List: Is an ordered and changeable collection. Duplicate members are allowed.
+Tuple: Is an ordered and unchangeable collection. Duplicate members are allowed.
+Set: Is an unordered and un-indexed collection. There are no duplicate members.
+Dictionary: Is an unordered, variable and indexed collection. There are no duplicate members.
+"""
+demo_list = ["Apple", "Banana", "Orange", "Watermelon", "Grape", "Lemon", "Mango"]
+print(demo_list)
+print(type(demo_list))
+print(demo_list[1])  # Get list member by index
+print(demo_list[-1])  # Get list member by index
+print(demo_list[2:5])  # Get list member by index range, left included right excluded
